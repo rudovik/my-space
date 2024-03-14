820848fd61eb371f281f46a6bee153a05e43fd6b
rudovik <flashwrite@ukr.net>
2024-03-14 20:24:19 +0200

    Changes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,15 +10,15 @@ interface Props {
   params: { slug: string }
 }
 
-export async function generateStaticParams() {
-  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
-    (res) => res.json()
-  )
+// export async function generateStaticParams() {
+//   const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
+//     (res) => res.json()
+//   )
 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }))
-}
+//   return posts.map((post) => ({
+//     slug: post.slug,
+//   }))
+// }
 
 export default async function BlogPostPage({ params }: Props) {
   const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
