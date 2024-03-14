05581e27f14d43b87d7146f0379a45a1c6e7c77c
rudovik <flashwrite@ukr.net>
2024-03-14 20:10:55 +0200

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+export const dynamic = "force-static"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "About Us",
+  description: "We are a social medai company",
+}
+
+export default async function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>We are a social media company</p>
+    </main>
+  )
+}
